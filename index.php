cff24e2bd23966e8ffb14cbc3adb0a5f2c950707
ALLEGRE Jérôme <jerome@lagriffe-digitale.fr>
2021-12-25 17:49:17 +0100

    Premiers fichiers
    theme.json, screenshot et index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+if ( current_user_can( 'activate_plugins' ) ) {
+	esc_html_e( 'This theme was built for the WordPress experimental full-site-editing feature. You need to install and activate the Gutenberg plugin to make it work.', 'miaou' );
+}
